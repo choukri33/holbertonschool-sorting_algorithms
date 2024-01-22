e28d7ce16993735dbd0345e797c8b357301dac1c
choukri33 <7646@holbertonstudents.com>
2024-01-22 02:01:04 -0800

    add 1-main.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,23 +2,24 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *node1, *node2;
-    if (list == NULL || *list == NULL)
-        return;
-    node1 = (*list)->next;
-    node2 = *list;
-    while (node1)
-    {
-        while (node1->prev)
-        {
-            if (node1->n < node1->prev->n)
-            {
-                node2 = node1->prev;
-                if (node2->prev)
-                    node2->prev->next = node1;
-                node1->prev = node2->prev;
-                if (node1->next)
-                    node1->next->prev = node2;
+	listint_t *node1, *node2;
+
+	if (list == NULL || *list == NULL)
+	return;
+	node1 = (*list)->next;
+	node2 = *list;
+	while (node1)
+	{
+	while (node1->prev)
+	{
+	if (node1->n < node1->prev->n)
+	{
+	node2 = node1->prev;
+	if (node2->prev)
+	node2->prev->next = node1;
+	node1->prev = node2->prev;
+	if (node1->next)
+	node1->next->prev = node2;
                 node2->next = node1->next;
                 node2->prev = node1;
                 node1->next = node2;
